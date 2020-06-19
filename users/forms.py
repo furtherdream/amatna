3ce1db0c884405eeacc8a30f038e28054e0da190
SeungHo Han <furtherdream.ceo@gmail.com>
2020-06-19 12:06:38 +0900

    이메일 로그인 (인증X)

--- users/forms.py
+++ users/forms.py
@@ -12,14 +12,65 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput(attrs={"placeholder": "비밀번호를 입력해 주세요."}),
     )
 
+    def clean(self):
+        # 클린을 사용하면 그 필드에 직접 에러를 추가해주면 됨.
+        # clean_password(), clean_username() 이렇데 할거면 그냥 error를 raise한면 됨
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+                # clean()을 썼다면 무조건 cleaned_data를 리턴해줘야 함
+            else:
+                self.add_error("password", forms.ValidationError("Password id wrong"))
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("email", "nickname")
+
+    password = forms.CharField(
+        label="패스워드",
+        widget=forms.PasswordInput(attrs={"placeholder": "비밀번호를 입력해 주세요."}),
+    )
+    password1 = forms.CharField(
+        label="패스워드 확인",
+        widget=forms.PasswordInput(attrs={"placeholder": "비밀번호를 확인해 주세요."}),
+    )
+
     def clean_email(self):
         email = self.cleaned_data.get("email")
         try:
-            models.User.objects.get(username=email)
-            return email
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with that email")
         except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
+            return email
 
-    def clean_password(self):
-        print("clean_password")
+    def clean_nickname(self):
+        nickname = self.cleaned_data.get("nickname")
+        try:
+            models.User.objects.get(nickname=nickname)
+            raise forms.ValidationError("User already exists with that nickname")
+        except models.User.DoesNotExist:
+            return nickname
 
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password comfirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
--- users/views.py
+++ users/views.py
@@ -1,15 +1,48 @@
 from django.views import View
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm()
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-        return render(request, "users/login.html", {"form": form})
+    # FormView에서는 get이나 post를 쓰지 않는다.
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "email": "furtherdream.ceo@gmail.com",
+        "nickname": "뻗어드림",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
