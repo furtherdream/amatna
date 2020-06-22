7007cd62bd1935d6f113e119d1346781a73a4945
SeungHo Han <furtherdream.ceo@gmail.com>
2020-06-22 19:52:37 +0900

    메인 페이지 _1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      h: {
+        "80": "80",
+      },
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+      },
+      borderRadius: {
+        xl: "1.5rem",
+      },
+      minHeight: {
+        "50vh": "50vh",
+        "75vh": "75vh",
+      },
+    },
   },
   variants: {},
   plugins: [],
