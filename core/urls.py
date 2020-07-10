0865a2bdd756810f1f72b6a9c34aad9e55e5cf06
SeungHo Han <furtherdream.ceo@gmail.com>
2020-07-10 16:59:41 +0900

    robots.txt 생성 및 url 연결

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from restaurants import views as restaurant_views
+from django.views.generic import TemplateView
 
 app_name = "core"
 # include() 안에 namespace 를 사용하면 app_name 을 지정해 줘야 한다.(namespace = app_name)
 
-urlpatterns = [path("", restaurant_views.main_views, name="home")]
+urlpatterns = [
+    path("", restaurant_views.main_views, name="home"),
+    path(
+        'robots.txt/',
+        TemplateView.as_view(
+            template_name="robots.txt", content_type='text/plain')),
+]
