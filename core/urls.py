7e65bbd91c226382ce3989fe58a23f018f29db45
SeungHo Han <furtherdream.ceo@gmail.com>
2020-07-30 18:11:07 +0900

    ads.txt 업로드

--- core/urls.py
+++ core/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
         'robots.txt/',
         TemplateView.as_view(
             template_name="robots.txt", content_type='text/plain')),
+    path(
+        'ads.txt/',
+        TemplateView.as_view(
+            template_name="ads.txt", content_type='text/plain')),
 ]
